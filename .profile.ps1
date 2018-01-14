fe820bf81010643b352710e7bad33d3334bd7ea7
Aofusa <5000enPCinAOFUSA@gmail.com>
2018-01-14 17:31:55 +0900

    [Add] sudo and su command

--- .profile.ps1
+++ .profile.ps1
@@ -12,4 +12,38 @@ Set-PSReadlineKeyHandler -Key 'Ctrl+n' -Function HistorySearchForward
 Set-PSReadlineKeyHandler -Key 'Ctrl+a' -Function BeginningOfLine
 Set-PSReadlineKeyHandler -Key 'Ctrl+e' -Function EndOfLine
 
+# Re-Define Pause command using any keys
+function Pause
+{
+    if ($psISE) {
+        $null = Read-Host 'Press Enter Key...'
+    }
+    else {
+        Write-Host "Press Any Key..."
+        (Get-Host).UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") | Out-Null
+    }
+}
+
+# Define sudo and su command
+function Invoke-CommandRunAs
+{
+    $cd = (Get-Location).Path
+    $commands = "Set-Location $cd; Write-Host `"[Administrator] $cd> $args`"; $args; Pause; exit"
+    $bytes = [System.Text.Encoding]::Unicode.GetBytes($commands)
+    $encodedCommand = [Convert]::ToBase64String($bytes)
+    Start-Process powershell.exe -Verb RunAs -ArgumentList "-NoExit","-encodedCommand",$encodedCommand
+}
+
+Set-Alias sudo Invoke-CommandRunAs
+
+function Start-RunAs
+{
+    $cd = (Get-Location).Path
+    $commands = "Set-Location $cd; (Get-Host).UI.RawUI.WindowTitle += `" [Administrator]`""
+    $bytes = [System.Text.Encoding]::Unicode.GetBytes($commands)
+    $encodedCommand = [Convert]::ToBase64String($bytes)
+    Start-Process powershell.exe -Verb RunAs -ArgumentList "-NoExit","-encodedCommand",$encodedCommand
+}
+
+Set-Alias su Start-RunAs
 
